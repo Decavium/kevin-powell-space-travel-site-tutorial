b31467c264a75ffdafa3e2983c787895bc76ced2
Leo Williams <leokabirwilliams@gmail.com>
2024-02-13 14:04:34 +1100

    Finished design system.

--- src/app/ui/numbers.tsx
+++ src/app/ui/numbers.tsx
@@ -11,12 +11,13 @@ export default function Numbers({ numbers }: Props) {
     const isSelected = (index: number) => index === 0 ? true : false;
 
     return (
-        <div className="number-indicators grid fs-600" style={{ fontFamily:"--ff-serif"}}>
+        <div className="number-indicators grid" style={{ fontFamily:"--ff-serif"}}>
             {numbers.map((numberButton, index) => {
                 return (
                     <button
                         aria-selected={isSelected(index)}
                         key={numberButton.name}
+                        className="fs-600 text-white"
                     >
                         <span className="sr-only">{numberButton.name}</span><p>{index + 1}</p>
                     </button>
